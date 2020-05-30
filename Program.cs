bea7fef08c9492761585635882708fc1b227ec14
Amr Badawy <abadawi@elm.sa>
2020-05-30 07:53:04 +0300

    Minor code enhancements & Set StartupObject

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 ﻿using CamundaClient;
 using CamundaClient.Dto;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
 using System.Threading;
@@ -11,31 +10,30 @@ namespace FlowingTripBookingSaga
 {
     class Program
     {
-
-        private static string logo =
+        private static readonly string logo =
                 "   ____                                _         ____  ____  __  __ \n" +
                 "  / ___|__ _ _ __ ___  _   _ _ __   __| | __ _  | __ )|  _ \\|  \\/  |\n" +
                 " | |   / _` | '_ ` _ \\| | | | '_ \\ / _` |/ _` | |  _ \\| |_) | |\\/| |\n" +
                 " | |__| (_| | | | | | | |_| | | | | (_| | (_| | | |_) |  __/| |  | |\n" +
                 "  \\____\\__,_|_| |_| |_|\\__,_|_| |_|\\__,_|\\__,_| |____/|_|   |_|  |_|\n";
 
-        private static int pollingIntervalInMilliseconds = 5;
-        private static int pollingNumberOfTasks = 100;
-        private static int pollingLockTimeInMs = 5 * 60 * 1000;
-        private static int pollingMaxDegreeOfParallelism = 1;
+        private static readonly int pollingIntervalInMilliseconds = 5;
+        private static readonly int pollingNumberOfTasks = 100;
+        private static readonly int pollingLockTimeInMs = 5 * 60 * 1000;
+        private static readonly int pollingMaxDegreeOfParallelism = 1;
         private static Timer pollingTimer;
 
-        private static string workerId = "worker1";
+        private static readonly string workerId = "worker1";
         private static IDictionary<string, Action<ExternalTask>> workers = new Dictionary<string, Action<ExternalTask>>();
         private static CamundaEngineClient camunda;
 
-        private static void Main(string[] args)
+        private static void Main()
         {
 
             Console.WriteLine(logo + "\n\n" + "Deploying models and start workers.\n\nPRESS ANY KEY TO STOP WORKERS.\n\n");
 
-            camunda = new CamundaEngineClient(
-                new System.Uri("http://localhost:8080/engine-rest/engine/default/"), null, null);
+            var uri = new Uri("http://localhost:8080/engine-rest/engine/default/");
+            camunda = new CamundaEngineClient(uri, null, null);
 
             // Alternative way of doing it: Search assembly and automatically deploy all models to Camunda and start all found workers
             // camunda.Startup();
@@ -57,37 +55,44 @@ namespace FlowingTripBookingSaga
             camunda.Shutdown(); // Stop Task Workers
         }
 
-        private static void DeployModel() { 
+        private static void DeployModel()
+        {
             camunda.RepositoryService.Deploy("trip-booking", new List<object> {
                 FileParameter.FromManifestResource(Assembly.GetExecutingAssembly(), "FlowingTripBookingSaga.Models.FlowingTripBookingSaga.bpmn") });
         }
 
         private static void RegisterWorker()
         {
-            registerWorker("book-hotel", externalTask => {
+            RegisterWorker("book-hotel", externalTask =>
+            {
                 Console.WriteLine("Book hotel now..."); // e.g. by calling a REST endpoint
                 camunda.ExternalTaskService.Complete(workerId, externalTask.Id);
             });
-            registerWorker("cancel-hotel", externalTask => {
+            RegisterWorker("cancel-hotel", externalTask =>
+            {
                 Console.WriteLine("Cancelling hotel now...");
                 camunda.ExternalTaskService.Complete(workerId, externalTask.Id);
             });
-            registerWorker("book-car", externalTask => {
+            RegisterWorker("book-car", externalTask =>
+            {
                 Console.WriteLine("Book car now...");
                 camunda.ExternalTaskService.Complete(workerId, externalTask.Id);
                 //camunda.ExternalTaskService.Failure(workerId, externalTask.Id, "could not connect", (externalTask.Retries ?? 3) - 1, 5000);
             });
-            registerWorker("cancel-car", externalTask => {
+            RegisterWorker("cancel-car", externalTask =>
+            {
                 Console.WriteLine("Cancelling car now...");
                 camunda.ExternalTaskService.Complete(workerId, externalTask.Id);
-            });       
-            registerWorker("book-flight", externalTask => {
+            });
+            RegisterWorker("book-flight", externalTask =>
+            {
                 Console.WriteLine("Reserving flight now...");
                 //camunda.ExternalTaskService.Complete(workerId, externalTask.Id);
                 //Console.WriteLine("Oh no - we hit a glitch!");
                 camunda.ExternalTaskService.Error(workerId, externalTask.Id, "BookingFailed");
             });
-            registerWorker("cancel-flight", externalTask => {
+            RegisterWorker("cancel-flight", externalTask =>
+            {
                 Console.WriteLine("Cancelling flight now...");
                 camunda.ExternalTaskService.Complete(workerId, externalTask.Id);
             });
@@ -104,9 +109,10 @@ namespace FlowingTripBookingSaga
         {
             var tasks = camunda.ExternalTaskService.FetchAndLockTasks(workerId, pollingNumberOfTasks, workers.Keys, pollingLockTimeInMs, null);
             Parallel.ForEach(
-                tasks, 
+                tasks,
                 new ParallelOptions { MaxDegreeOfParallelism = pollingMaxDegreeOfParallelism },
-                (externalTask) => {
+                (externalTask) =>
+                {
                     workers[externalTask.TopicName](externalTask);
                 });
 
@@ -114,11 +120,9 @@ namespace FlowingTripBookingSaga
             pollingTimer.Change(pollingIntervalInMilliseconds, Timeout.Infinite);
         }
 
-        private static void registerWorker(string topicName, Action<ExternalTask> workerFunction)
+        private static void RegisterWorker(string topicName, Action<ExternalTask> workerFunction)
         {
             workers.Add(topicName, workerFunction);
         }
-    
     }
-    
 }
